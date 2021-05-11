eb0062e998a462b51cdc14f8540da0ffa6b37178
orhanors <orsorhan1@gmail.com>
2021-05-11 19:00:08 +0300

    travis file added

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# Build docker images
+# We use 2 tag --> 1.latest 2.latest git commit SHA which is stored as .env variable in travis.yml file
+
+docker build -t orhanors/linkedin-client:latest -t orhanors/linkedin-client:$SHA -f ./frontend/Dockerfile ./frontend
+docker build -t orhanors/linkedin-server:latest -t orhanors/linkedin-server:$SHA -f ./backend/Dockerfile ./backend
+
+#Push images to docker hub
+docker push orhanors/linkedin-client:latest
+docker push orhanors/linkedin-client:$SHA
+
+docker push orhanors/linkedin-server:latest
+docker push orhanors/linkedin-server:$SHA
+
+#Apply kubectl
+kubectl apply -f k8s
+
+#Set new image to deployments
+#If we push new image kubernetes will be using latest image that we pushed
+
+kubectl set image deployments/li-client-deployment client=orhanors/linkedin-client:$SHA
+kubectl set image deployments/li-server-deployment server=orhanors/linkedin-server:$SHA
